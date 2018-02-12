a8f7bb69748c297ab08750bf2f16721f95df6e4a
Susanna <numfar85@gmail.com>
2018-02-12 22:26:48 +0100

    Databas!

--- src/python/src/file_coordinates_extractor.py
+++ src/python/src/file_coordinates_extractor.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import re
+import sys
 
 class TrainStopp(object):
 
@@ -24,3 +24,13 @@ class FileCoordinatesExtractor(object):
                 stop = TrainStopp(cols[0],int(cols[1]),int(cols[2]))
                 route.stops.append(stop)
         return route
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    ext = FileCoordinatesExtractor()
+    route = ext.extract_route(filename)
+    print route.name
+    print route.stops[0].name
+    print route.stops[0].x
+    print route.stops[1].name
+    print route.stops[1].x
---
+++ src/python/src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='routy',
+    packages=['routy'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
